572f8a4ee1083efa944ec8b6d59425dbf05e6728
System administrator <root@cr48-nixos.lan>
2015-02-17 20:15:53 -0700

    Updated cr48 config. Working on specifying a synthing version

--- computers/cr48-nixos/configuration.nix
+++ computers/cr48-nixos/configuration.nix
@@ -9,8 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../standard-packages.nix
-      ../../standard-env.nix
-      ../../standard-nixpath.nix
+      ../../syncthing.nix
     ];
 
   # Use the GRUB 2 boot loader.
@@ -44,46 +43,56 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
+  services.xserver.exportConfiguration = true;
   # services.xserver.xkbOptions = "eurosign:e";
 
   # Enable the KDE Desktop Environment.
   services.xserver.displayManager.kdm.enable = true;
   services.xserver.desktopManager.kde4.enable = true;
-  services.xserver.synaptics.enable = true;
-  services.xserver.synaptics.palmDetect = true;
-  services.xserver.synaptics.dev = "/devices/platform/i8042/serio4/input/input15";
-  services.xserver.synaptics.additionalOptions =
-  ''
-  Option "Palm Pressure" "235.0"
-  Option "Touchpad Stack Version" "1"
-  Option "Integrated Touchpad" "1"
-  Option "Pressure Calibration Offset" "-23.2"
-  Option "Pressure Calibration Slope" "1.21"
-  Option "Input Queue Delay" "0.017"
-  # Bounds overrides:
-  Option "Active Area Left" "1217"
-  Option "Active Area Right" "5733"
-  Option "Active Area Top" "1061"
-  Option "Active Area Bottom" "4798"
-  # Palm overrides:
-  Option "Palm Pressure" "235.0"
-  # SemiMT overrides:
-  Option "SemiMT Correcting Filter Enable" "1"
-  # Scroll Stationary Finger Distance overrides:
-  Option "Scroll Stationary Finger Max Distance" "20.0"
-  # Tapping Finger Min Separation Distance overrides:
-  Option "Tap Min Separation" "0.0"
-  # Max Pressure Change overrides:
-  Option "Max Allowed Pressure Change Per Sec" "2200.0"
-  Option "Max Hysteresis Pressure Per Sec" "1200.0"
-  # Double Fling Buffer Depth due to 2x Interpolation
-  Option "Fling Buffer Depth" "20"
-  Option "Two Finger Vertical Close Distance Thresh" "35.0"
-  Option "Fling Buffer Suppress Zero Length Scrolls" "0"
-  '';
-  #services.xserver.videoDriver = "intel";
+  services.xserver.synaptics = {
+    enable = true;
+#    identifier = "touchpad mario";
+      tapButtons = false;
+      palmDetect = true;
+      #dev = "/devices/platform/i8042/serio4/input/input15";
+      additionalOptions = ''
+        Option "Palm Pressure" "235.0"
+        Option "Touchpad Stack Version" "1"
+        Option "Integrated Touchpad" "1"
+        Option "Pressure Calibration Offset" "-23.2"
+        Option "Pressure Calibration Slope" "1.21"
+        Option "Input Queue Delay" "0.017"
+        # Bounds overrides:
+        Option "Active Area Left" "1217"
+        Option "Active Area Right" "5733"
+        Option "Active Area Top" "1061"
+        Option "Active Area Bottom" "4798"
+        # Palm overrides:
+        Option "Palm Pressure" "235.0"
+        # SemiMT overrides:
+        Option "SemiMT Correcting Filter Enable" "1"
+        # Scroll Stationary Finger Distance overrides:
+        Option "Scroll Stationary Finger Max Distance" "20.0"
+        # Tapping Finger Min Separation Distance overrides:
+        Option "Tap Min Separation" "0.0"
+        # Max Pressure Change overrides:
+        Option "Max Allowed Pressure Change Per Sec" "2200.0"
+        Option "Max Hysteresis Pressure Per Sec" "1200.0"
+        # Double Fling Buffer Depth due to 2x Interpolation
+        Option "Fling Buffer Depth" "20"
+        Option "Two Finger Vertical Close Distance Thresh" "35.0"
+        Option "Fling Buffer Suppress Zero Length Scrolls" "0"
+
+      '';
+};
+  services.xserver.videoDrivers = [ "intel" "vesa"];
   services.xserver.multitouch.enable = true;
   services.xserver.multitouch.ignorePalm = true;
+  environment.systemPackages = [
+    pkgs.xorg.xf86inputsynaptics
+    #pkgs.xf86-video-intel
+  ];
+  time.timeZone = "America/Denver";
 
   boot.kernelPackages = pkgs.linuxPackages_3_18;
   nixpkgs.config.packageOverrides = pkgs:
--- syncthing.nix
+++ syncthing.nix
@@ -4,6 +4,21 @@ with pkgs.lib;
   imports = [
     users/shaun.nix
   ];
+
+
+  config.environment.systemPackages = [
+  (pkgs.lib.overrideDerivation pkgs.syncthing (attrs: {
+     version = "0.10.23";
+     name = "syncthing-0.10.23";
+     src = pkgs.fetchgit {
+    url = "git://github.com/syncthing/syncthing.git";
+    rev = "refs/tags/v0.10.23";
+    sha256 = "0zkrirkfzlh0iwqasm0k8z18hbb8zcx6vn6b5vb3bcpiwpcq66hz";
+  };
+
+  }))
+  ];
+
   
   config.services =
   {
